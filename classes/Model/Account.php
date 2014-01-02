b8b9833288d231a5f1adb0ca3714398ade58f10b
Mauricio S Otta <mauricio.otta@in.sert.co.in>
2014-01-01 16:50:23 -0800

    Lowercase email and id for accounts

--- classes/Model/Account.php
+++ classes/Model/Account.php
@@ -30,6 +30,14 @@ class Model_Account extends Model_Abstract
 
 	public function _before_save(&$data = array())
 	{
+		if (! empty($data['_id']))
+		{
+			$data['_id'] = strotolower($data['_id']);
+		}
+		if (! empty($data['email']))
+		{
+			$data['email'] = strotolower($data['email']);
+		}
 		unset($data['password1'], $data['password2'], $data['remember_me']);
 		parent::_before_save($data);
 	}
