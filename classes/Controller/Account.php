3580c672e7f64cbea787a7af14c17a41c183b6ca
Maurício S. Otta <mauricio@portnumber53.com>
2014-07-08 23:37:26 -0700

    Added Controller_Account and started templating profile form

---
+++ classes/Controller/Account.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Date: 7/8/14
+ * Time: 11:33 PM
+ * Something meaningful about this file
+ *
+ */
+
+class Controller_Account extends Controller_Common_Account {}
---
+++ views/account/profile.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Date: 7/8/14
+ * Time: 11:32 PM
+ * Something meaningful about this file
+ *
+ */
+
+echo View::factory('account/default/profile')->render();
