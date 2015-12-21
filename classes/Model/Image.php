ee58ed0976ab1580760469cc2f6bb00d599e4214
Mauricio S. Otta <mauricio@portnumber53.com>
2015-12-20 17:22:56 -0800

    PSR-2

--- classes/Model/Image.php
+++ classes/Model/Image.php
@@ -1,44 +1,39 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-/**
- * Date: 11/24/13
- * Time: 10:11 PM
- * Something meaningful about this file
- *
- */
 
+/**
+ * Class Model_Image
+ */
 class Model_Image extends Model_Abstract
 {
-	public static $_table_name = 'image';
-	public static $_primary_key = '_id';
+    public static $_table_name = 'image';
+    public static $_primary_key = '_id';
 
-	public static $_columns = array(
-		'_id'           => '',
-		'url'           => '',
-		'object_id'     => 0,
-		'category_id'   => 0,
-		'status'        => '',
-		'name'          => '',
-		'description'   => '',
-		'created_at'    => '',
-		'modified_at'   => '',
-		'extra_json'    => '{}',
-	);
+    public static $_columns = array(
+        '_id' => '',
+        'url' => '',
+        'object_id' => 0,
+        'category_id' => 0,
+        'status' => '',
+        'name' => '',
+        'description' => '',
+        'created_at' => '',
+        'modified_at' => '',
+        'extra_json' => '{}',
+    );
 
-	public static $_json_columns = array(
-		'tags'			=> '',
-	);
+    public static $_json_columns = array(
+        'tags' => '',
+    );
 
-	public function _before_save(&$data = array())
-	{
-		parent::_before_save($data);
-		if (empty($data['object_id']))
-		{
-			$data['object_id'] = Model_Sequence::nextval();
-		}
-		if ( ! empty($data['object_id']) && ! empty($data['name']))
-		{
-			$data['url'] = '/' . $data['object_id'] . '-' . URL::title($data['name']) . '/';
-		}
+    public function _before_save(&$data = array())
+    {
+        parent::_before_save($data);
+        if (empty($data['object_id'])) {
+            $data['object_id'] = Model_Sequence::nextval();
+        }
+        if (!empty($data['object_id']) && !empty($data['name'])) {
+            $data['url'] = '/' . $data['object_id'] . '-' . URL::title($data['name']) . '/';
+        }
 
-	}
+    }
 }
