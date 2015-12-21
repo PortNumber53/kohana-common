20e6eddd1bb0aa0f72c8a7e692d71a8ed426a7cf
Mauricio S. Otta <mauricio@portnumber53.com>
2015-12-20 16:38:26 -0800

    PSR-2

--- classes/Controller/Common/Core/Dynimage.php
+++ classes/Controller/Common/Core/Dynimage.php
@@ -5,22 +5,22 @@
  */
 class Controller_Common_Core_Dynimage extends Controller_Website
 {
-	public $auto_render = FALSE;
+    public $auto_render = false;
 
-	public function action_get()
-	{
-		//print_r($_SERVER);
-		$request = $this->request->param('request');
-		$type = $this->request->param('type');
-		$full_request = ($request === '/') ? $request : "$request.$type";
+    public function action_get()
+    {
+        //print_r($_SERVER);
+        $request = $this->request->param('request');
+        $type = $this->request->param('type');
+        $full_request = ($request === '/') ? $request : "$request.$type";
 
-		$file_name = DATAPATH . DIRECTORY_SEPARATOR . 'upload' . DIRECTORY_SEPARATOR . $full_request;
+        $file_name = DATAPATH . DIRECTORY_SEPARATOR . 'upload' . DIRECTORY_SEPARATOR . $full_request;
 
-		$mime_type = mime_content_type ( $file_name );
-		$this->response->headers('content-type', $mime_type);
-		echo file_get_contents($file_name);
+        $mime_type = mime_content_type($file_name);
+        $this->response->headers('content-type', $mime_type);
+        echo file_get_contents($file_name);
 
 
-	}
+    }
 
 }
