bb8d5316b7af92f711847e8840fcd898093ebe7d
Mauricio S. Otta <mauricio@portnumber53.com>
2015-12-20 23:25:54 -0800

    PSR-2 and image route with resizing/cropping options

--- init.php
+++ init.php
@@ -6,12 +6,12 @@
  */
 if (!Route::$cache) {
     Route::set('backend-actions', 'backend(/<controller>(/<action>(/<request>)))', array(
-            'request' => '[a-zA-Z0-9_/\-]+',
-        ))->defaults(array(
-            'directory' => 'Backend',
-            'controller' => 'Dashboard',
-            'action' => 'Main',
-        ));
+        'request' => '[a-zA-Z0-9_/\-]+',
+    ))->defaults(array(
+        'directory' => 'Backend',
+        'controller' => 'Dashboard',
+        'action' => 'Main',
+    ));
     /*
 	Route::set('dashboard', 'dashboard',
 		array(
@@ -25,26 +25,26 @@ if (!Route::$cache) {
     */
 
     Route::set('service-actions', 'service(/<controller>(/<action>(/<request>)))', array(
-            'request' => '[a-zA-Z0-9_/\-]+',
-        ))->defaults(array(
-            'directory' => 'Service',
-            'controller' => 'Check',
-            'action' => 'Unique',
-        ));
+        'request' => '[a-zA-Z0-9_/\-]+',
+    ))->defaults(array(
+        'directory' => 'Service',
+        'controller' => 'Check',
+        'action' => 'Unique',
+    ));
 
     Route::set('account-actions', '<action>(/<request>)', array(
-            'request' => '[a-zA-Z0-9_/\-]+',
-            'action' => '(profile|login|signup|logout|reset|forgot|settings|public)',
-        ))->defaults(array(
-            'controller' => 'Account',
-            'action' => 'profile',
-        ));
+        'request' => '[a-zA-Z0-9_/\-]+',
+        'action' => '(profile|login|signup|logout|reset|forgot|settings|public)',
+    ))->defaults(array(
+        'controller' => 'Account',
+        'action' => 'profile',
+    ));
 
     Route::set('html-content', '(<request>.<type>(<override>))', array(
-            'request' => '[a-zA-Z0-9_/\-]+',
-            'type' => '(html|shtml)',
-            'override' => '(:edit)',
-        ))->filter(function ($route, $params, $request) {
+        'request' => '[a-zA-Z0-9_/\-]+',
+        'type' => '(html|shtml)',
+        'override' => '(:edit)',
+    ))->filter(function ($route, $params, $request) {
         // Prefix the method to the action name
         if (!empty($params['override']) && $params['override'] == ':edit') {
             $params['action'] = 'edit';
@@ -53,46 +53,50 @@ if (!Route::$cache) {
         //$params['action'] = strtolower($request->method()).'_'.$params['action'];
         return $params; // Returning an array will replace the parameters
     })->defaults(array(
-            'controller' => 'Content',
-            'action' => 'view',
-            'request' => '/',
-            'type' => 'html',
-        ));
+        'controller' => 'Content',
+        'action' => 'view',
+        'request' => '/',
+        'type' => 'html',
+    ));
 
-    Route::set('image-actions', '<request>.<type>', array(
-            'request' => '[a-zA-Z0-9_/\-]+',
-            'type' => '(jpg|jpeg|png|gif)',
-        ))->defaults(array(
-            'controller' => 'Dynimage',
-            'action' => 'get',
-        ));
-
+    Route::set('image-actions', 'image/(<action>/<width>/<height>/<method>/)<pictureid>-<request>.<type>', array(
+        'action' => '[a-zA-Z_/\-]+',
+        'width' => '[0-9]+',
+        'height' => '[0-9]+',
+        'method' => '(crop)',
+        'pictureid' => '[0-9]+',
+        'request' => '[a-zA-Z0-9_/\-]+',
+        'type' => '(jpg|jpeg|png|gif)',
+    ))->defaults(array(
+        'controller' => 'Dynimage',
+        'action' => 'view',
+    ));
 
     Route::set('sitemap', 'sitemap/<name>(:<page>).<format>', array(
-            'name' => '[a-zA-Z0-9_/\-]+',
-            'page' => '([0-9]+|count)',
-            'format' => '(xml|txt)',
-        ))->defaults(array(
-            'controller' => 'Sitemap',
-            'action' => 'generate',
-        ));
+        'name' => '[a-zA-Z0-9_/\-]+',
+        'page' => '([0-9]+|count)',
+        'format' => '(xml|txt)',
+    ))->defaults(array(
+        'controller' => 'Sitemap',
+        'action' => 'generate',
+    ));
 
     Route::set('seo-robots', 'robots.txt')->defaults(array(
-            'controller' => 'Robots',
-            'action' => 'index',
-        ));
+        'controller' => 'Robots',
+        'action' => 'index',
+    ));
 
 
     //Legacy actions
     Route::set('blog-actions', 'gallery(/<id>(-(<slug>)))(:<page>)', array(
-            'id' => '[0-9]+',
-            'page' => '[0-9]+',
-            'slug' => '[a-z0-9_\-\']+',
-        ))->defaults(array(
-            'controller' => 'Blog',
-            'format' => 'Blog',
-            'page' => 1,
-            'action' => 'view',
-        ));
+        'id' => '[0-9]+',
+        'page' => '[0-9]+',
+        'slug' => '[a-z0-9_\-\']+',
+    ))->defaults(array(
+        'controller' => 'Blog',
+        'format' => 'Blog',
+        'page' => 1,
+        'action' => 'view',
+    ));
 
 }
