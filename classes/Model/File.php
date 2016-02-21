b2f2c7fef2b08647d8e52fd6e05018c8b50f49a2
Mauricio S. Otta <mauricio@portnumber53.com>
2016-02-20 17:53:49 -0800

    PSR-2

--- classes/Model/File.php
+++ classes/Model/File.php
@@ -1,30 +1,30 @@
 <?php defined('SYSPATH') or die('No direct script access.');
+
 /**
  * Date: 11/16/13
  * Time: 1:09 PM
  * Something meaningful about this file
  *
  */
-
 class Model_File extends Model_Abstract
 {
-	public static $_table_name = 'file';
-	public static $_primary_key = '_id';
+    public static $_table_name = 'file';
+    public static $_primary_key = '_id';
 
-	public static $_columns = array(
-		'_id'           => '',
-		'object_id'     => 0,
-		'category_id'   => 0,
-		'status'        => '',
-		'name'          => '',
-		'description'   => '',
-		'created_at'    => '',
-		'modified_at'   => '',
-		'extra_json'    => '{}',
-	);
+    public static $_columns = array(
+        '_id' => '',
+        'object_id' => 0,
+        'category_id' => 0,
+        'status' => '',
+        'name' => '',
+        'description' => '',
+        'created_at' => '',
+        'modified_at' => '',
+        'extra_json' => '{}',
+    );
 
-	public static $_json_columns = array(
-		'tags'			=> '',
-	);
+    public static $_json_columns = array(
+        'tags' => '',
+    );
 
 }
