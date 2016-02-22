d825e80fefc2ae6fb8f857a80c734699e40206ba
Mauricio S. Otta <mauricio@portnumber53.com>
2016-02-21 22:55:23 -0800

    Code styling

--- views/account/login.php
+++ views/account/login.php
@@ -7,41 +7,43 @@
 
 ?><h1>Login</h1>
 
-<form class="form-horizontal json-form" role="form" method="post" action="<?php echo URL::Site(Route::get('account-actions')->uri(array('action'=>'login', )), TRUE); ?>">
-	<div class="form-group">
-		<label for="inputEmail1" class="col-lg-2 control-label">Email</label>
-		<div class="col-lg-10">
-			<input type="email" class="form-control" id="inputEmail1" placeholder="Email" name="email">
-		</div>
-	</div>
-	<div class="form-group">
-		<label for="inputPassword1" class="col-lg-2 control-label">Password</label>
-		<div class="col-lg-10">
-			<input type="password" class="form-control" id="inputPassword1" placeholder="Password" name="password">
-		</div>
-	</div>
-	<div class="form-group">
-		<div class="col-lg-offset-2 col-lg-10">
-			<div class="checkbox">
-				<label title="(for a week)">
-					<input type="checkbox" name="remember_me" value="1"> Remember me
-				</label>
-			</div>
-		</div>
-	</div>
-	<div class="form-group">
-		<div class="col-lg-offset-2 col-lg-10">
-			<button type="submit" name="action" class="btn btn-default">Sign in</button>
-		</div>
-	</div>
-	<div class="form-group">
-		<div class="col-lg-offset-2 col-lg-10">
-			<div class="checkbox">
-				<label>
-					<a href="<?php echo URL::Site(Route::get('account-actions')->uri(array('action'=>'signup', )), TRUE); ?>"> Sign up here</a>
-				</label>
-			</div>
-		</div>
-	</div>
+<form class="form-horizontal json-form" role="form" method="post"
+      action="<?php echo URL::Site(Route::get('account-actions')->uri(array('action' => 'login',)), TRUE); ?>">
+    <div class="form-group">
+        <label for="inputEmail1" class="col-lg-2 control-label">Email</label>
+        <div class="col-lg-10">
+            <input type="email" class="form-control" id="inputEmail1" placeholder="Email" name="email">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPassword1" class="col-lg-2 control-label">Password</label>
+        <div class="col-lg-10">
+            <input type="password" class="form-control" id="inputPassword1" placeholder="Password" name="password">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-lg-offset-2 col-lg-10">
+            <div class="checkbox">
+                <label title="(for a week)">
+                    <input type="checkbox" name="remember_me" value="1"> Remember me
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-lg-offset-2 col-lg-10">
+            <button type="submit" name="action" class="btn btn-default">Sign in</button>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-lg-offset-2 col-lg-10">
+            <div class="checkbox">
+                <label>
+                    <a href="<?php echo URL::Site(Route::get('account-actions')->uri(array('action' => 'signup',)), TRUE); ?>">
+                        Sign up here</a>
+                </label>
+            </div>
+        </div>
+    </div>
 </form>
 
--- views/account/settings.php
+++ views/account/settings.php
@@ -9,36 +9,40 @@
 
 <form name="settings" method="post" action="<?php echo URL::site(Request::detect_uri(), TRUE); ?>">
 
-	Key: <input type="text" name="key" id="key" size="40" value="<?php echo empty($item_data['key']) ? '' : htmlentities($item_data['key']); ?>">
-	<br />
-	Value: <textarea name="value" id="value" cols="100" rows="10"><?php echo empty($item_data['value']) ? '' : htmlentities($item_data['value']); ?></textarea>
-	<br />
-	<br />
-	<button id="btnaction_new" class="form_submit button blue flat" class="form_submit button blue flat"> New Option </button>
-	<button id="btnaction_update" class="form_submit button blue flat" class="form_submit button blue flat"> Update Settings </button>
+    Key: <input type="text" name="key" id="key" size="40"
+                value="<?php echo empty($item_data['key']) ? '' : htmlentities($item_data['key']); ?>">
+    <br/>
+    Value: <textarea name="value" id="value" cols="100"
+                     rows="10"><?php echo empty($item_data['value']) ? '' : htmlentities($item_data['value']); ?></textarea>
+    <br/>
+    <br/>
+    <button id="btnaction_new" class="form_submit button blue flat" class="form_submit button blue flat"> New Option
+    </button>
+    <button id="btnaction_update" class="form_submit button blue flat" class="form_submit button blue flat"> Update
+        Settings
+    </button>
 
 </form>
 
 
-
 <ul>
-	<?php
-	if (is_array($data['data']))
-	{
-		foreach ($data['data'] as $key=>&$item)
-		{
-		?>
-			<li><a href="<?php echo URL::site(Route::get('account-actions')->uri(array('action'=>'settings', 'request'=>$key, )), TRUE); ?>"><?php echo $key; ?></a></li>
-		<?php
-		}
-	}
-	?>
+    <?php
+    if (is_array($data['data'])) {
+        foreach ($data['data'] as $key => &$item) {
+            ?>
+            <li>
+                <a href="<?php echo URL::site(Route::get('account-actions')->uri(array('action' => 'settings', 'request' => $key,)), TRUE); ?>"><?php echo $key; ?></a>
+            </li>
+            <?php
+        }
+    }
+    ?>
 </ul>
 
 <script>
-$(document).ready(function() {
-	$("#btnaction_new").on('click', function() {
-		document.location="<?php echo URL::Site(route::get('account-actions')->uri(array('action'=>'settings', )), TRUE); ?>";
-	})
-})
+    $(document).ready(function () {
+        $("#btnaction_new").on('click', function () {
+            document.location = "<?php echo URL::Site(route::get('account-actions')->uri(array('action' => 'settings',)), TRUE); ?>";
+        })
+    })
 </script>
